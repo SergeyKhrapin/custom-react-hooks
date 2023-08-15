c635050382bfa9abf666d681606466073d27a1e6
SergeyKhrapin <khrapins@gmail.com>
2023-08-15 13:19:27 +0300

    small tweaks

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,36 @@
 import {useCallback, useEffect, useState} from "react"
 import useDebounce from "../customHooks/useDebounce"
 
+class Shape {
+  constructor() {}
+  calculateSquare() {
+
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    console.log("constructor");
+    super();
+    this.radius = radius;
+  }
+
+  static _ = (() => {
+    console.log("static block");
+  })()
+
+  diameter = this.radius * 2;
+
+  calculateSquare() {
+    console.log('this', this);
+    return 2 * 3.14 * this.radius;
+  }
+}
+
+const circle = new Circle(10);
+console.log("circle", circle instanceof Shape);
+console.log("calculateSquare", circle.calculateSquare());
+
 const Country = () => {
   const [country, setCountry] = useState("")
 
@@ -8,7 +38,7 @@ const Country = () => {
     return await fetch(`https://restcountries.com/v2/name/${value}`)
   }, 500)
 
-  const fetchSuggestions = useCallback((val) => debounce(val), [])
+  const fetchSuggestions = useCallback((val) => debounce(val), [debounce])
 
   useEffect(() => {
     fetchSuggestions(country)
@@ -17,14 +47,17 @@ const Country = () => {
   const handleChange = (e) => setCountry(e.target.value)
 
   return (
-    <form>
-      <input onChange={handleChange} placeholder="Search country" />
-      <ul id="suggestions">
-        {countries?.map(country => (
-          <li key={country.alpha2Code}>{country.name}</li>
-        ))}
-      </ul>
-    </form>
+    <>
+      <h2>useDebounce</h2>
+      <form>
+        <input onChange={handleChange} placeholder="Search country" />
+        <ul id="suggestions">
+          {countries?.map(country => (
+            <li key={country.alpha2Code}>{country.name}</li>
+          ))}
+        </ul>
+      </form>
+    </>
   )
 }
 
